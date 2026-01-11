10103804681bb96f16782f2d1e61d0d1258aa39d
James Ennis <ennismjames@gmail.com>
2026-01-10 23:25:43 -0600

    init

---
+++ src/main/java/frc/robot/subsystems/Swerve.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems;
+
+public class Swerve {
+    public Swerve(int driveMotorID, int turnMotorID){
+
+    }
+
+    public Swerve(int driveMotorID, int turnMotorID, double x, double y){
+
+    }
+}
